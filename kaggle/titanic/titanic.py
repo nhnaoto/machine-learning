203d54e311df2e86530fc27b6abfb9edd2637427
nhnaoto <nh.for.app@gmail.com>
2017-11-26 09:46:36 +0900

    モデル評価にSVMを追加

--- kaggle/titanic/titanic.py
+++ kaggle/titanic/titanic.py
@@ -43,6 +43,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.neural_network import MLPClassifier
+from sklearn import svm
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
@@ -53,9 +54,11 @@ pipe_logistic = Pipeline([('scl',StandardScaler()),('est',LogisticRegression(ran
 pipe_rf = Pipeline([('scl',StandardScaler()),('est',RandomForestClassifier(random_state=1))])
 pipe_gb = Pipeline([('scl',StandardScaler()),('est',GradientBoostingClassifier(random_state=1))])
 pipe_mlp = Pipeline([('scl',StandardScaler()),('est',MLPClassifier(hidden_layer_sizes=(100,3), max_iter=500, random_state=1))])
+pipe_svm = Pipeline([('scl',StandardScaler()),('est',svm.SVC())])
 
-pipe_names = ['KNN','Logistic','RandomForest','GradientBoosting','MLP']
-pipe_lines = [pipe_knn, pipe_logistic, pipe_rf, pipe_gb, pipe_mlp]
+
+pipe_names = ['KNN','Logistic','RandomForest','GradientBoosting','MLP','SVM']
+pipe_lines = [pipe_knn, pipe_logistic, pipe_rf, pipe_gb, pipe_mlp, pipe_svm]
 
 # 交差検証
 from sklearn.model_selection import cross_val_score
@@ -63,28 +66,47 @@ for (i,pipe) in enumerate(pipe_lines):
     scores = cross_val_score(pipe, X, y.as_matrix().ravel(), cv=10, scoring='accuracy')
     print('%s: %.3f'%(pipe_names[i],scores.mean()))
 
-# ->0.831
-pipe = pipe_gb
-
-# グリッドサーチ
-from sklearn.model_selection import GridSearchCV
-param_grid_gbc = {'est__n_estimators':[50,100],'est__subsample':[0.8, 1.0]}
-param = param_grid_gbc
-
-best_estimator = []
-print('----------------------------------------------------------------------------------------------')
-print('探索空間:%s' % param)
-gs = GridSearchCV(estimator=pipe, param_grid=param, scoring='accuracy', cv=3)
-gs = gs.fit(X, y.as_matrix().ravel())
-best_estimator.append(gs.best_estimator_) 
-print('Best Score %.6f\n' % gs.best_score_) 
-print('Best Model: %s' % gs.best_estimator_)
-
-pipe_gb_best = Pipeline([('scl',StandardScaler()),('est',GradientBoostingClassifier(random_state=1, subsample=0.8, n_estimators=100))])
-pipe = pipe_gb_best
+# ->0.835
 
 
-#fit
+
+# In[ ]:
+
+# print(SVC().get_params().keys())
+# # グリッドサーチ
+# from sklearn.model_selection import GridSearchCV
+# param_grid_gb = {'est__n_estimators':[50,100],'est__subsample':[0.8, 1.0]}
+# param_grid_svm = {'gamma': [0.001, 0.0001], 'kernel': ['rbf']}
+
+# pipes = [pipe_svm, pipe_gb]
+# params = [param_grid_svm, param_grid_gb]
+
+# # best_estimator = []
+# for elem in zip(pipes, params):
+#     print('----------------------------------------------------------------------------------------------')
+#     pipe, param = elem[0], elem[1]
+#     print('探索空間:%s' % param)
+#     gs = GridSearchCV(estimator=pipe, param_grid=param, scoring='accuracy', cv=3)
+#     gs = gs.fit(X, y.as_matrix().ravel())
+#     best_estimator.append(gs.best_estimator_) 
+#     print('Best Score %.6f\n' % gs.best_score_)
+#     print('Best Model: %s' % gs.best_estimator_)
+
+# print('探索空間:%s' % param_grid_svm)
+# gs = GridSearchCV(estimator=pipe_svm, param_grid=param_grid_svm, scoring='accuracy', cv=3)
+# gs = gs.fit(X, y.as_matrix().ravel())
+# best_estimator.append(gs.best_estimator_) 
+# print('Best Score %.6f\n' % gs.best_score_) 
+# print('Best Model: %s' % gs.best_estimator_)
+
+
+
+# In[ ]:
+
+# fit 
+# pipe_gb_best = Pipeline([('scl',StandardScaler()),('est',GradientBoostingClassifier(random_state=1, subsample=0.8, n_estimators=100))])
+# pipe = pipe_gb_best
+pipe = pipe_svm
 pipe.fit(X, y.as_matrix().ravel())
 
 
@@ -120,7 +142,7 @@ B["Survived"] = pred
 B = df.loc[:, ['PassengerId', 'Survived']];
 B.to_csv("./output/prediction.csv",index=False)
 
-# -> 0.78468
+# -> score 0.79904
 
 
 # In[ ]:
